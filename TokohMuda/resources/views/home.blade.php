bead0129bc9ef2403a94c51553dc6b0122eb5b7e
Ammar084 <sayyid.ammar2002@gmail.com>
2022-12-27 13:04:47 +0700

    update

--- TokohMuda/resources/views/home.blade.php
+++ TokohMuda/resources/views/home.blade.php
@@ -9,32 +9,29 @@
 </head>
 
 <link rel="stylesheet" href="app.css">
-<link rel="stylesheet" href="normalize.css">
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<body class="body">
-	<div class="navbar-logo-left wf-section">
-		<div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" role="banner" class="navbar-logo-left-container shadow-three w-nav">
-			<div class="container">
-				<div class="navbar-wrapper">
-					<a href="#" class="navbar-brand w-nav-brand"><img src="assets/Logo_Tokoh_Muda.jpg" loading="lazy" height="80" srcset="" alt="logo" class="image-4"></a>
-					<nav role="navigation" class="nav-menu-wrapper w-nav-menu">
-						<ul role="list" class="nav-menu-two w-list-unstyled">
-							<li>
-								<a href="#" class="nav-link">Home</a>
-							</li>
-							<li>
-								<a href="#" class="nav-link">Tokoh Muda</a>
-							</li>
-							<li>
-								<a href="" class="nav-link">Rankings</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
+<body>
+	<nav>
+		<div class="logo">
+			<h4>Tokoh Muda</h4>
 		</div>
-	</div>
+		
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Tokoh Muda</a></li>
+			<li><a href="">Rangkings</a></li>
+		</ul>
+
+		<div class="menu-toggle">
+			<input type="checkbox" >
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</nav>
+
 	<section class="hero-heading-center wf-section">
 		<div class="container">
 			<h1 class="centered-heading margin-bottom-32px">Tokoh Muda Kerinci</h1>
@@ -42,15 +39,62 @@
 				<div class="hero-split">
 					<h1 class="margin-bottom-24px" style="font-size: 40px; padding: 0;">HEI FIGUR MUDA!</h1>
 					<p class="margin-bottom-24px">Kami Mendorong Seluruh Generasi Muda Kerinci - Sungai Penuh Untuk Berkarya, 100 Figur Muda Inovatif, 100rb Genereasi Muda Majukan Kerinci - Sungai Penuh Ikuti Cerita Kami</p>
-					<a href="#" class="button-primary w-button">Start Voting</a>
 				</div>
-				<div class="hero-split"><img src="assets/Tokoh Muda.jpg" loading="lazy" srcset="" width="100%" alt="TokohMuda" class="shadow-two"></div>
+				<div class="hero-split"><img src="assets/asd.png" loading="lazy" srcset="" width="100%" alt="TokohMuda" class="shadow-two"></div>
 			</div>
 		</div>
 	</section>
+
+	<div class="title">
+      <h1>Tokoh Muda Terkini</h1>
+    </div>
+
+    <div class="wrapper">
+      <div class="container2">
+        <div class="photo">
+          <img src="assets/berita1.png" alt="">
+        </div>
+        <div class="card">
+          <h2>Juara 1 MTQ Ke-51</h2>
+          <p>Kaum Muda</p>
+        </div>
+      </div>
+
+      <div class="container2">
+        <div class="photo">
+          <img src="assets/berita2.png" alt="">
+        </div>
+        <div class="card">
+          <h2>Pemuda Pelopor</h2>
+          <p>Kaum Muda</p>
+        </div>
+      </div>
+
+      <div class="container2">
+        <div class="photo">
+          <img src="assets/berita3.png" alt="">
+        </div>
+        <div class="card">
+          <h2>Beasiswa Oyisi</h2>
+          <p>Tokoh Muda</p>
+        </div>
+      </div>
+      
+      <div class="container2">
+        <div class="photo">
+          <img src="assets/berita4.png" alt="">
+        </div>
+        <div class="card">
+          <h2>Workshop Digital Marketing</h2>
+          <p>Tokoh Muda</p>
+        </div>
+      </div>
+    </div>
+
 	<section class="team-circles wf-section">
 		<div class="container">
-			<h2 class="centered-heading">Tokoh Muda Kerinci</h2>
+			<h2 class="centered-heading-2">Tokoh Muda Kerinci</h2>
+			<p class="centered-subheading">Tokoh Muda Membangun Negeri</p>
 			<div class="team-grid">
 				<div id="" class="team-card"><img src="assets/bg_ahmad.jpg" loading="lazy" alt="" class="team-member-image">
 					<div class="team-member-name">Ahmad Hafizd</div>
@@ -187,18 +231,28 @@
 			</div>
 		</div>
 	</section>
+
+
 	<section class="footer-subscribe wf-section">
 		<div class="container">
 			<div class="footer-wrapper-three">
-							
-				<div class="footer-bottom">
-					<div class="footer-copyright">@ 2022 Tokoh Muda - Voting Tokoh Muda. All right reserved</div>
-					<div href="#" class="footer-legal-block">
-						<a href="https://www.instagram.com/tokohmudakerinci/?hl=id" class="footer-legal-link">@tokohmudakerinci</a>
-					</div>
+				<div class="footer-social-block-three">
+					<a href="#" class="footer-social-block-three w-inline-block" loading="lazy" alt="" class="image-6">
+						<div class="text-block">@tokohmudakerinci</div>
+					</a>
+				</div>
+			</div>
+			<div class="footer-divider-two"></div>
+			<div class="footer-bottom">
+				<div class="footer-copyright">@ 2002 Tokoh Muda. All right reserved</div>
+				<div class="footer-legal-block">
+					<a href="" class="footer-legal-link">#tokohmudakerinci</a>
+					<a href="" class="footer-legal-link">#2024panggunganakmuda</a>
 				</div>
 			</div>
 		</div>
 	</section>
+
+	<script src="script.js"></script>
 </body>
 </html>
