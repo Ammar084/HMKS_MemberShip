70ee5f047d47f17175bb8e15d7b2c47426358795
Ammar084 <sayyid.ammar2002@gmail.com>
2022-12-27 16:08:13 +0700

    update

--- TokohMuda/resources/js/app.js
+++ TokohMuda/resources/js/app.js
@@ -1,8 +1,1 @@
 require('./bootstrap');
-
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', function(){
-	nav.classList.toggle('slide');
-});
