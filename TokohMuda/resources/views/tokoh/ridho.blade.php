4632e04defacee81c97c21f1cd70e463a2db505d
Ammar084 <sayyid.ammar2002@gmail.com>
2022-12-28 20:02:11 +0700

    update

---
+++ TokohMuda/resources/views/tokoh/ridho.blade.php
@@ -0,0 +1,20 @@
+<img src="assets/bg_ridho.jpg" width="100%">
+	        <h1>M. Ridho Syahlindra</h1>
+	        <p>Deskripsi</p>
+	        <p>Projek yang sedang berjalan</p>
+	        <div>
+	        	<h3>Social Media</h3>
+	        	<a href="https://www.instagram.com/ridhosyahlindra/" target="_blank">Instagram</a>
+	        	<a href="https://www.facebook.com/m.m.ridho.3" target="_blank">Facebook</a>
+	        </div>
+	        <div>
+	        	<h3>Tautan</h3>
+	        	
+	        	<div>
+	        		<a href="">Wikipedia</a>
+	        	</div>
+	        	<div>
+	        		<a href="">Projek</a>	
+	        	</div>
+	        	
+	        </div>
